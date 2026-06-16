3b59263dd96c90e81bde80403ab1096d4a20128e
ahmedsami019 <easami019192@gmail.com>
2026-06-16 22:49:21 +0600

    some ui change

--- src/Layouts/HomeLayout/HomeLayout.jsx
+++ src/Layouts/HomeLayout/HomeLayout.jsx
@@ -3,13 +3,15 @@ import Navbar from "../../Pages/Shared/Navbar/Navbar";
 import Footer from "../../Pages/Shared/Footer/Footer";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className="bg-base-100">
+      <section className="w-11/12 mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+      </section>
+    </div>
+  );
 };
 
 export default HomeLayout;
