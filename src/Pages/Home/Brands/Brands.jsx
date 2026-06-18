158ba66a73eaaf2a3bce1a24a1110395b8762c70
ahmedsami019 <easami019192@gmail.com>
2026-06-18 10:24:30 +0600

    added swiper for brands and better ui

---
+++ src/Pages/Home/Brands/Brands.jsx
@@ -0,0 +1,47 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import amazon from "../../../assets/brands/amazon.png";
+import amazon_vector from "../../../assets/brands/amazon_vector.png";
+import casio from "../../../assets/brands/casio.png";
+import moonstar from "../../../assets/brands/moonstar.png";
+import randstad from "../../../assets/brands/randstad.png";
+import star_people from "../../../assets/brands/start-people 1.png";
+import star from "../../../assets/brands/start.png";
+import { Autoplay } from "swiper/modules";
+
+const Brands = () => {
+  const allBrands = [
+    amazon,
+    amazon_vector,
+    casio,
+    moonstar,
+    randstad,
+    star_people,
+    star,
+  ];
+  return (
+    <div className="space-y-10">
+      <h2 className="text-3xl font-bold">
+        We've helped thousands of sales teams
+      </h2>
+
+      <Swiper
+      modules={[Autoplay]}
+        slidesPerView={5}
+        centeredSlides={true}
+        spaceBetween={30}
+        grabCursor={true}
+        autoplay={{ delay: 1200, disableOnInteraction: false }}
+        loop={true}
+      >
+        {allBrands.map((brand) => (
+          <SwiperSlide>
+            <img src={brand} alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Brands;
--- src/Pages/Home/HomePage/HomePage.jsx
+++ src/Pages/Home/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
+import Brands from "../Brands/Brands";
 import DeliveryOptions from "../DeliveryOptions/DeliveryOptions";
 import Services from "../Services/Services";
 import HeroBanner from "./HeroBanner/HeroBanner";
 
 const HomePage = () => {
     return (
-        <div className="space-y-10">
+        <div className="space-y-20">
             <HeroBanner/>
             <DeliveryOptions/>
             <Services/>
+            <Brands></Brands>
         </div>
     );
 };
